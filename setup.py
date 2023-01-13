5fc172051e0238f56ef557fc61a2f6e7a1fc09e4
Shreyans Maini <shreyans@oneassure.in>
2023-01-13 14:01:45 +0530

    updating setup.py to run pip install via git

--- setup.py
+++ setup.py
@@ -1,9 +1,35 @@
-#!/usr/bin/env python
-
-# This has only been added to allow editable dev installs, pyproject.toml replaces setup.py
-# e.g. pip install -e .
-
+# #!/usr/bin/env python
+#
+# # This has only been added to allow editable dev installs, pyproject.toml replaces setup.py
+# # e.g. pip install -e .
+#
+# import setuptools
+#
+# if __name__ == "__main__":
+#     setuptools.setup()
 import setuptools
+import distutils.text_file
+from pathlib import Path
+from typing import List
 
-if __name__ == "__main__":
-    setuptools.setup()
+
+def _parse_requirements(filename: str) -> List[str]:
+    return distutils.text_file.TextFile(filename=str(Path(__file__).with_name(filename))).readlines()
+
+
+required = _parse_requirements('requirements.txt')
+
+setuptools.setup(
+    name="fastapi_keycloak",
+    version="1.0.9",
+    author="1Verse",
+    description="FastAPI Keycloak",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "fastapi_keycloak"},
+    packages=setuptools.find_packages(where="fastapi_keycloak"),
+    install_requires=required
+    )
+
